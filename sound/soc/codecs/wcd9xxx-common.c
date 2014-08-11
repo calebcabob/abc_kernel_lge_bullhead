ae799eeed3260ab9fc89ce99915faa250e0b8277
Sudheer Papothi <spapothi@codeaurora.org>
2014-08-11 23:07:18 +0530

    ASoC: wcd9xxx: Changes for High performance mode
    Add changes for high performance audio playback on headset
    for better audio quality during 192KHz/24bit audio playback.
    
    CRs-Fixed: 706322
    Change-Id: Ie79293f24114d5d81a610186d24b44b1550dae9c
    Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

--- sound/soc/codecs/wcd9xxx-common.c
+++ sound/soc/codecs/wcd9xxx-common.c
@@ -558,9 +558,8 @@ void wcd9xxx_enable_high_perf_mode(struct snd_soc_codec *codec,
 					WCD9XXX_A_RX_HPH_BIAS_PA__POR);
 		snd_soc_write(codec, WCD9XXX_A_RX_HPH_L_PA_CTL, 0x48);
 		snd_soc_write(codec, WCD9XXX_A_RX_HPH_R_PA_CTL, 0x48);
-		if (uhqa_mode)
-			snd_soc_update_bits(codec, WCD9XXX_A_RX_HPH_CHOP_CTL,
-						0x20, 0x00);
+		snd_soc_update_bits(codec, WCD9XXX_A_RX_HPH_CHOP_CTL,
+					0x20, 0x00);
 		wcd9xxx_chargepump_request(codec, true);
 		wcd9xxx_enable_anc_delay(codec, true);
 		wcd9xxx_enable_buck(codec, clsh_d, false);
@@ -579,7 +578,7 @@ void wcd9xxx_enable_high_perf_mode(struct snd_soc_codec *codec,
 					WCD9XXX_A_RX_HPH_L_PA_CTL__POR);
 		snd_soc_write(codec, WCD9XXX_A_RX_HPH_R_PA_CTL,
 					WCD9XXX_A_RX_HPH_R_PA_CTL__POR);
-		snd_soc_write(codec, WCD9XXX_A_RX_HPH_BIAS_PA, 0x57);
+		snd_soc_write(codec, WCD9XXX_A_RX_HPH_BIAS_PA, 0x55);
 		wcd9xxx_enable_buck(codec, clsh_d, true);
 		wcd9xxx_chargepump_request(codec, false);
 		wcd9xxx_enable_anc_delay(codec, false);
